26dfa8dc7206bb727c448b20907752b6a601b111
Samuel Silva <samus355@gmail.com>
2022-05-13 17:21:44 -0300

    test: ensure CreateUser should call AddUserAccount with correct values

--- tests/presentation/controllers/create-user-controller.spec.ts
+++ tests/presentation/controllers/create-user-controller.spec.ts
@@ -2,6 +2,7 @@ import { CreateUserController } from '../../../src/presentation/controllers/crea
 import { MissingParamError } from '../../../src/presentation/errors/missing-param-error'
 import { AddUserAccount, AddUserAccountModel } from '../../../src/domain/usecases/AddUserAccount'
 import { UserModel } from '../../../src/domain/models/User'
+import { ServerError } from '../../../src/presentation/errors/server-error'
 
 type SutTypes = {
   sut: CreateUserController,
@@ -10,7 +11,7 @@ type SutTypes = {
 
 const makeAddUserAccount = () => {
   class AddUserAccountStub implements AddUserAccount {
-    add (account: AddUserAccountModel): UserModel {
+    async add (account: AddUserAccountModel): Promise<UserModel> {
       const fakeAccount = {
         id: 'valid_id',
         fullName: 'valid_fullName',
@@ -37,7 +38,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddUserController', () => {
-  test('should return 400 if no fullName is provided', () => {
+  test('should return 400 if no fullName is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -47,13 +48,13 @@ describe('AddUserController', () => {
         phoneNumber: 'any_phone_number'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('fullName'))
   })
 
-  test('should return 400 if no email is provided', () => {
+  test('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -63,13 +64,13 @@ describe('AddUserController', () => {
         phoneNumber: 'any_phone_number'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('should return 400 if no addressNumber is provided', () => {
+  test('should return 400 if no addressNumber is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -79,13 +80,13 @@ describe('AddUserController', () => {
         phoneNumber: 'any_phone_number'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('addressNumber'))
   })
 
-  test('should return 400 if no address is provided', () => {
+  test('should return 400 if no address is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -95,13 +96,13 @@ describe('AddUserController', () => {
         phoneNumber: 'any_phone_number'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('address'))
   })
 
-  test('should return 400 if no phoneNumber is provided', () => {
+  test('should return 400 if no phoneNumber is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -111,13 +112,13 @@ describe('AddUserController', () => {
         address: 'any_address',
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('phoneNumber'))
   })
 
-  test('should call AddUserAccount with correct values', () => {
+  test('should call AddUserAccount with correct values', async () => {
     const { sut, addUserAccountStub } = makeSut()
     const addSpy = jest.spyOn(addUserAccountStub, 'add')
     const httpRequest = {
@@ -130,7 +131,7 @@ describe('AddUserController', () => {
       }
     }
 
-    sut.handle(httpRequest)
+    await sut.handle(httpRequest)
     expect(addSpy).toHaveBeenCalledWith({
       fullName: 'any_fullName',
       email: 'any_email@mail.com',
@@ -138,5 +139,25 @@ describe('AddUserController', () => {
       addressNumber: 'any_address_number',
       phoneNumber: 'any_phone_number'
     })
+  })
+
+  test('should return 500 if AddUserAccount throws', async () => {
+    const { sut, addUserAccountStub } = makeSut()
+    jest.spyOn(addUserAccountStub, 'add').mockImplementationOnce(async () => {
+      return new Promise((resolve, rejects) => rejects(new Error()))
+    })
+    const httpRequest = {
+      body: {
+        fullName: 'any_fullName',
+        email: 'any_email@mail.com',
+        address: 'any_address',
+        addressNumber: 'any_address_number',
+        phoneNumber: 'any_phone_number'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
