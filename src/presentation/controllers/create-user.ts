0fef20af15bcf4f87d9bcaa77cb96378f52086a1
Samuel Silva <samus355@gmail.com>
2022-05-13 14:37:27 -0300

    feat: ensure CreateUser returns 400 if no address is provided

--- src/presentation/controllers/create-user.ts
+++ src/presentation/controllers/create-user.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from "../errors/missing-param-error"
 
 export class CreateUserController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['fullName', 'email', 'addressNumber']
+    const requiredFields = ['fullName', 'email', 'addressNumber', 'address']
     
     for (const fields of requiredFields) {
       if (!httpRequest.body[fields]) {
