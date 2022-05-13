c76dcde42e9312d540921d8dcdcfd7c871b4111b
Samuel Silva <samus355@gmail.com>
2022-05-13 16:54:20 -0300

    test: ensure CreateUser should call AddUserAccount with correct values

--- tests/presentation/controllers/create-user-controller.spec.ts
+++ tests/presentation/controllers/create-user-controller.spec.ts
@@ -1,9 +1,44 @@
 import { CreateUserController } from '../../../src/presentation/controllers/create-user'
 import { MissingParamError } from '../../../src/presentation/errors/missing-param-error'
+import { AddUserAccount, AddUserAccountModel } from '../../../src/domain/usecases/AddUserAccount'
+import { UserModel } from '../../../src/domain/models/User'
+
+type SutTypes = {
+  sut: CreateUserController,
+  addUserAccountStub: AddUserAccount,
+}
+
+const makeAddUserAccount = () => {
+  class AddUserAccountStub implements AddUserAccount {
+    add (account: AddUserAccountModel): UserModel {
+      const fakeAccount = {
+        id: 'valid_id',
+        fullName: 'valid_fullName',
+        email: 'valid_email',
+        address: 'valid_address',
+        addressNumber: 'valid_address_number',
+        phoneNumber: 'valid_phone_number'
+      }
+      return fakeAccount
+    }
+  }
+  
+  return new AddUserAccountStub()
+}
+
+const makeSut = (): SutTypes => {
+  const addUserAccountStub = makeAddUserAccount()
+  const sut = new CreateUserController(addUserAccountStub)
+
+  return {
+    sut,
+    addUserAccountStub
+  }
+}
 
 describe('AddUserController', () => {
   test('should return 400 if no fullName is provided', () => {
-    const sut = new CreateUserController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email',
@@ -19,7 +54,7 @@ describe('AddUserController', () => {
   })
 
   test('should return 400 if no email is provided', () => {
-    const sut = new CreateUserController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         fullName: 'any_fullName',
@@ -35,7 +70,7 @@ describe('AddUserController', () => {
   })
 
   test('should return 400 if no addressNumber is provided', () => {
-    const sut = new CreateUserController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         fullName: 'any_fullName',
@@ -51,7 +86,7 @@ describe('AddUserController', () => {
   })
 
   test('should return 400 if no address is provided', () => {
-    const sut = new CreateUserController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         fullName: 'any_fullName',
@@ -67,7 +102,7 @@ describe('AddUserController', () => {
   })
 
   test('should return 400 if no phoneNumber is provided', () => {
-    const sut = new CreateUserController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         fullName: 'any_fullName',
@@ -80,5 +115,28 @@ describe('AddUserController', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('phoneNumber'))
+  })
+
+  test('should call AddUserAccount with correct values', () => {
+    const { sut, addUserAccountStub } = makeSut()
+    const addSpy = jest.spyOn(addUserAccountStub, 'add')
+    const httpRequest = {
+      body: {
+        fullName: 'any_fullName',
+        email: 'any_email@mail.com',
+        address: 'any_address',
+        addressNumber: 'any_address_number',
+        phoneNumber: 'any_phone_number'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith({
+      fullName: 'any_fullName',
+      email: 'any_email@mail.com',
+      address: 'any_address',
+      addressNumber: 'any_address_number',
+      phoneNumber: 'any_phone_number'
+    })
   })
 })
